f8d0b4fcb09de50e2489a3bd5c40c983467d576f
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-06-15 02:58:00 +0600

    a hook with tanstack query added

--- src/hooks/useUsers.jsx
+++ src/hooks/useUsers.jsx
@@ -1,10 +1,15 @@
+import { useQuery } from '@tanstack/react-query'
 
 const useUsers = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const { isLoading, data: users = [] } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/users')
+            return res.json();
+        },
+    })
+
+    return users, isLoading;
 };
 
 export default useUsers;
