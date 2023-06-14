ccbcf08c590ee5980978281a13aa26ada1a2bd50
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-06-14 23:15:46 +0600

    sample links added

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,5 +1,5 @@
-import { Outlet } from "react-router-dom";
-
+import { NavLink, Outlet } from "react-router-dom";
+import { FaShoppingCart, FaWallet, FaCalendarAlt, FaHome, FaUtensils, FaBook, FaUsers } from 'react-icons/fa';
 
 const Dashboard = () => {
 
@@ -9,7 +9,7 @@ const Dashboard = () => {
             <div className="drawer-content flex flex-col items-center justify-center">
                 {/* Page content here */}
                 <Outlet></Outlet>
-                
+
                 <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open Menu</label>
 
             </div>
@@ -17,8 +17,24 @@ const Dashboard = () => {
                 <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
                 <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
                     {/* Sidebar content here */}
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
+
+                    <li><NavLink to="/dashboard/adminhome"><FaHome></FaHome> Admin Home</NavLink></li>
+                    <li><NavLink to="/dashboard/addClass"> <FaUtensils></FaUtensils> Add an Class</NavLink></li>
+                    <li><NavLink to="/dashboard/manageClass"><FaWallet></FaWallet> Manage Class</NavLink></li>
+                    <li><NavLink to="/"><FaBook></FaBook> Manage Enrolled</NavLink></li>
+                    <li><NavLink to="/dashboard/allusers"><FaUsers></FaUsers> All Users</NavLink></li>
+
+
+                    <li><NavLink to="/dashboard/userhome"><FaHome></FaHome> User Home</NavLink></li>
+                    <li><NavLink to="/"><FaCalendarAlt></FaCalendarAlt> Enrolled</NavLink></li>
+                    <li><NavLink to="/"><FaWallet></FaWallet> Payment History</NavLink></li>
+                    <li><NavLink to="/dashboard/myclass"><FaShoppingCart></FaShoppingCart> My Class</NavLink></li>
+
+
+                    <div className="divider"></div>
+                    <li><NavLink to="/"><FaHome></FaHome> Home</NavLink> </li>
+                    <li><NavLink to="/"><FaHome></FaHome> Home</NavLink> </li>
+                    <li><NavLink to="/"><FaHome></FaHome> Home</NavLink> </li>
                 </ul>
 
             </div>
