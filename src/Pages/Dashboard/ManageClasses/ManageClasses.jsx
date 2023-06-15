56a66ed20fb3fa91c6705103992884d3f4dce724
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-06-16 01:51:11 +0600

    feedback implemented

--- src/Pages/Dashboard/ManageClasses/ManageClasses.jsx
+++ src/Pages/Dashboard/ManageClasses/ManageClasses.jsx
@@ -1,9 +1,13 @@
 import Swal from "sweetalert2";
 import useClasses from "../../../hooks/useClasses";
 import ClassRow from "./ClassRow";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const ManageClasses = () => {
     const [classes, refetch] = useClasses();
+    const [clickedClass, setClickedClass] = useState([]);
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
 
     const handleApprove = aClass => {
         fetch(`http://localhost:5000/classes/approved/${aClass._id}`, {
@@ -45,7 +49,42 @@ const ManageClasses = () => {
             })
     }
 
-    
+    const handleFeedback = aClass => {
+        setClickedClass(aClass);
+        window.feedbackModal.showModal();
+    }
+
+    const onsubmit = (data) => {
+        console.log('aaa', clickedClass);
+        const feedback = data.feedback;
+        console.log(feedback);
+
+        // const feedbackInfo = { feedback };
+        // console.log(feedbackInfo);
+
+
+        fetch(`http://localhost:5000/classes/feedback/${clickedClass._id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(feedback)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.modifiedCount) {
+                    reset();
+                    Swal.fire({
+                        position: 'center',
+                        icon: 'success',
+                        title: `Feedback sent for ${clickedClass.className} success!`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+            })
+    }
 
     return (
         <div className='w-[85%] mx-auto'>
@@ -79,6 +118,23 @@ const ManageClasses = () => {
                     </tbody>
                 </table>
             </div>
+
+            <dialog id="feedbackModal" className="modal">
+                <form onSubmit={handleSubmit(onsubmit)} method="dialog" className="modal-box w-11/12 max-w-2xl">
+                    <h3 className="font-bold text-lg">Send Feedback!</h3>
+                    <textarea {...register("feedback", {
+                        required: true,
+                        minLength: 10,
+                        maxLength: 250
+                    })} name="feedback" rows="4" className="p-4 mt-2 border-2 rounded-lg border-gray w-full resize-none"></textarea>
+                    {errors.feedback?.type === 'required' && <p className="text-red-600 text-sm mt-2">Feedback is required</p>}
+                    {errors.feedback?.type === 'minLength' && <p className="text-red-600 text-sm mt-2">Feedback must be 10 characters</p>}
+                    {errors.feedback?.type === 'maxLength' && <p className="text-red-600 text-sm mt-2">feedback must be less than 250 characters</p>}
+                    <div className="modal-action mt-2">
+                        <button className="btn bg-primary-color hover:bg-secondary-color border-primary-color hover:border-secondary-color">Submit</button>
+                    </div>
+                </form>
+            </dialog>
         </div >
     );
 };
