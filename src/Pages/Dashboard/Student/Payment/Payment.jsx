c7fba0fe3b817c0f793fc240c44fe267f8897505
S M Arifur Rahman <smarifurrahman82@gmail.com>
2023-06-25 22:26:31 +0600

    stripe payment gateway displayed initially with pk

--- src/Pages/Dashboard/Student/Payment/Payment.jsx
+++ src/Pages/Dashboard/Student/Payment/Payment.jsx
@@ -1,9 +1,18 @@
+import { loadStripe } from "@stripe/stripe-js";
 import PageHeader from "../../../Shared/PageHeader/PageHeader";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckoutForm from "./CheckoutForm";
+
+// TODO: Provide publishable key
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_STRIPE_PK);
 
 const Payment = () => {
     return (
         <div>
             <PageHeader title='Payment System'></PageHeader>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm></CheckoutForm>
+            </Elements>
         </div>
     );
 };
